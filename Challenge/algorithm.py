347d53f12ec7b743823f63b87946a71a9a3e11a1
Javier Ardila <32044695+renderjaviii@users.noreply.github.com>
2019-09-22 17:15:44 -0500

    Añadido algoritmo genetico, encuentra la solucion con el mejor SCORE

--- Challenge/algorithm.py
+++ Challenge/algorithm.py
@@ -19,6 +19,14 @@ def printTable(table):
             print(int(table[i][j]), end=" ")
         print()
 
+def copyTable(table):
+    table_copy = numpy.zeros((len(table), len(table)))
+    for i in range(len(table)):
+        for j in range(len(table)):
+            table_copy[i][j] = table[i][j]
+
+    return table_copy
+
 
 class Movement():
     def __init__(self, previousPos, currentPos):
@@ -34,10 +42,9 @@ class ColorGroup():
         self.colorNumber = colorNumber
 
     def __str__(self):
-        print("color:", self.colorNumber, ", size: ",len(self.positions),"\npositions-> ", end="")
+        print("color:", self.colorNumber, ", size: ", len(self.positions), "\npositions-> ", end="")
         for position in self.positions:
             print(position, end=" ")
-        print("\n")
 
 
 def isInvalidMovement(movement, n):
@@ -143,8 +150,10 @@ def getPositionGroup(group_list, position):
         if position in group_list[i].positions:
             return i
 
-def touchPosition(table, group_list, position):
-    print("Touching a position {}...".format(position))
+def touchPosition(table, position, debug):
+    if debug: print("Touching a position {}...".format(position))
+
+    group_list = makeGroups(table)
     group_position = getPositionGroup(group_list, position)
     group = group_list[group_position].positions
 
@@ -198,14 +207,15 @@ def getTouchPuntuation(N):
 
 
 # Genetic algorithm
-def generateNextState(current_table):
-    current_group_list = makeGroups(table)
-
+def generateNextState(current_table, current_playlist):
+    current_group_list = makeGroups(current_table)
     index_group = random.randint(0, len(current_group_list) - 1)
     position_to_touch = current_group_list[index_group].positions[0]
-    current_score = touchPosition(table, current_group_list, position_to_touch)
+    current_score = touchPosition(current_table, position_to_touch, True)
 
-    return current_table, makeGroups(table), current_score
+    current_playlist.append(position_to_touch)
+
+    return current_table, makeGroups(current_table), current_score
 
 def getLargerGroupIndex(group_list):
     max = -1
@@ -237,33 +247,71 @@ def hasOnlySingletons(group_list):
 def gameWon(group_list):
     return len(group_list) == 0
 
-def getGameState(i, group_list):
-    resp = "in " + str(i + 1) + " iterations"
-
+def getGameState(group_list, score, debug):
     if gameWon(group_list):
-        print("Game WIN", resp)
+        if debug: print("Game WIN with: {} points".format(score))
         return 0
     elif hasOnlySingletons(group_list):
-        print("Game finished by singletons state", resp)
+        if debug: print("Game finished by singletons state, with: {} points".format(score))
         return 1
     else:
-        print("NOT have finished", resp)
+        if debug: print("NOT have finished, current points: {}".format(score))
 
     return -1
 
-def solveGame(table):
-    max_score = -1
-    max_position = -1
-    n_times = 3
 
-    for i in range(n_times):
-        table, group_list, score = generateNextState(table)
+def solveGame(table,  n_attempts, n_tables, n_projections):
 
-        printState(table, group_list, score)
-        game_state = getGameState(i, group_list)
+    best_score = -1
+    best_general_playlist = []
+    for i in range(n_attempts):
+        print("\nAttempt #{}".format(i + 1))
+        print("---------------------------")
+        score_and_playlist_attempt = {}
 
-        if game_state == 0 or game_state == 1:
-            break
+        for j in range(n_tables):
+            if len(best_general_playlist) > 0:
+                print("\nDescendant of the best previous generation")
+                table_attempt, score_attempt = projectGameUsingPlaylist(copyTable(table), best_general_playlist)
+                playlist_attempt = best_general_playlist
+
+                print("Score inherited: {}\nPlaylist inherited -> {}".format(score_attempt, playlist_attempt))
+
+                game_state = getGameState(group_list_attempt, score_attempt, False)
+                if game_state == 0 or game_state == 1:
+                    break
+            else:
+                print("\nPossible initial population")
+                score_attempt = 0
+                playlist_attempt = []
+                table_attempt = copyTable(table)
+
+            for k in range(n_projections):
+                table_attempt, group_list_attempt, score = generateNextState(table_attempt, playlist_attempt)
+                score_attempt += score
+
+                game_state = getGameState(group_list_attempt, score_attempt, True)
+                if game_state == 0 or game_state == 1:
+                    break
+
+            score_and_playlist_attempt[score_attempt] = playlist_attempt
+
+        max_score_attempt = max(score_and_playlist_attempt.keys())
+        if max_score_attempt > best_score:
+            best_score = max_score_attempt
+            best_general_playlist = score_and_playlist_attempt[max_score_attempt]
+
+    print("BEST SCORE FOUND", best_score)
+    print(best_general_playlist)
+
+
+def projectGameUsingPlaylist(table, playlist):
+    score = 0
+    for position in playlist:
+        score += touchPosition(table, position, False)
+
+    return table, score
+
 
 def printState(table, group_list, score):
     print("SCORE:", score)
@@ -272,9 +320,11 @@ def printState(table, group_list, score):
         print(g.colorNumber, "->", g.positions)
 
 
-table = ([2, 2, 1, 1], [1, 2, 1, 2], [1, 2, 1, 2], [2, 2, 1, 2])
+#table = ([2, 2, 1, 1], [1, 2, 1, 2], [1, 2, 1, 2], [2, 2, 1, 2])
+table = generateTable(10)
 printTable(table)
-solveGame(table)
+
+solveGame(table, n_attempts=2, n_tables=5, n_projections=1)
 
 """Greedy
 groupList = makeGroups(table)
@@ -287,3 +337,17 @@ while len(groupList) > 0:
     score += getTouchPuntuation(amount_boxes)
 
 print("Score-> {}".format(score))"""
+
+#Peso = (cantidad de fichas un color) / (cantidad total de fichas)
+
+# Tocar las fichas de menor peso en orden ascendente
+# Tocar las fichas que estén imposibilitando la unión de 2 grupos que formarían un gran peso
+# Elegir el color con mayor peso e intentar romper la mayor cantidad de las otras
+# fichas, buscando que todas las fichas del color elegido queden juntas
+
+
+
+
+
+
+
